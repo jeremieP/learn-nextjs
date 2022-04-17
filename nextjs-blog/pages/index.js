20844c311af0f30c3686c77a53770079e171dcff
jeremieP <jeremie.pereira@gmail.com>
2022-04-18 00:30:43 +0200

    First navigation between pages

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,17 +1,21 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Learn Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className="title">
+        Read{' '}
+        <Link href="/posts/first-post">
+          <a>this page!</a>
+        </Link>
+      </h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
